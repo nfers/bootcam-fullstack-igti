1cd0c3fa5a1bf8cac422098efe533474b10cd1b6
Nayara Ferreira <ncy.fers@gmail.com>
2020-08-02 17:56:35 -0300

    implementado metodo push nas rotas de conta

--- my-bank-api/src/routes/account.js
+++ my-bank-api/src/routes/account.js
@@ -1,8 +1,26 @@
 import express from 'express';
+import { promises as fs } from 'fs';
 
+const { readFile, writeFile } = fs;
 
 const accountRouter = express.Router();
 
-console.log('ok')
+accountRouter.post('/', async (req, res) => {
+ try {
+  let account = req.body
+  const data = JSON.parse(await readFile('account.json'));
 
+  account.id = data.nextId;
+
+  data.nextId++;
+
+  data.accounts.push(account)
+
+  await writeFile('account.json', JSON.stringify(data))
+  console.log(data)
+  res.status(200).send({ result: true, data: [{ id: account.id, name: account.name }] })
+ } catch (error) {
+  res.status(404).send({ error: error.message })
+ }
+})
 export default accountRouter;
