f970a9e1efe4f861671c407d6bd80f456da207a2
Nayara Ferreira <ncy.fers@gmail.com>
2020-08-06 11:51:10 -0300

    implementacao de rotas e documentacao com swagger

--- Bootcamp-IGTI-Desafio-Modulo-2/src/index.js
+++ Bootcamp-IGTI-Desafio-Modulo-2/src/index.js
@@ -1,1 +1,13 @@
-node_modules
+import express from 'express';
+import routes from './routes/index.js';
+import swaggerUi from 'swagger-ui-express';
+import { swaggerDocument } from './document.js';
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/grades', routes);
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.listen(3333);
